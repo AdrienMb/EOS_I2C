c2cfa020e3c27fa83aad74ab52b0d165192140d4
Milan Jaritz <milan.jaritz@gmail.com>
2018-10-29 11:28:10 +0100

    hello2

--- Sensor.cpp
+++ Sensor.cpp
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <math.h>
 #include <stdio.h>
+#include <bitset>
 
 using namespace std;
 
@@ -12,7 +13,13 @@ namespace exploringBB {
 Sensor::~Sensor() {}
 
 void Sensor::displayData() {
-    this->write(54);
+    int x = this->write(0x27);
+    unsigned char *y = this->readDevice(5);
+
+    for(int i = 0; i < 4; i++) {
+        std::cout << std::bitset<8>(y[i]) << " ";
+        printf("%d\n",y[i]);
+    }
 }
 
 Sensor::Sensor(unsigned int I2CBus, unsigned int I2CAddress):
