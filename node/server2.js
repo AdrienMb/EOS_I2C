6ecab08ade410fa2efbffc9c4ec7464a4e0c5846
Milan Jaritz <milan.jaritz@gmail.com>
2018-11-17 12:55:19 +0100

    add node modules add all the sensor values

--- node/server2.js
+++ node/server2.js
@@ -7,7 +7,7 @@ var path = require('path');
 // Initialize the server on port 8888
 var server = http.createServer(function (req, res) {
     // requesting files
-    var file = '.'+((req.url=='/')?'/index.html':req.url);
+    var file = '.' + ((req.url == '/') ? '/index.html' : req.url);
     var fileExtension = path.extname(file);
     var contentType = 'text/html';
     // Uncoment if you want to add css to your web page
@@ -15,17 +15,17 @@ var server = http.createServer(function (req, res) {
     if(fileExtension == '.css'){
         contentType = 'text/css';
     }*/
-    fs.exists(file, function(exists){
-        if(exists){
-            fs.readFile(file, function(error, content){
-                if(!error){
+    fs.exists(file, function (exists) {
+        if (exists) {
+            fs.readFile(file, function (error, content) {
+                if (!error) {
                     // Page found, write content
-                    res.writeHead(200,{'content-type':contentType});
+                    res.writeHead(200, { 'content-type': contentType });
                     res.end(content);
                 }
             });
         }
-        else{
+        else {
             // Page not found
             res.writeHead(404);
             res.end('Page not found');
@@ -37,27 +37,92 @@ var server = http.createServer(function (req, res) {
 // SEE KNOWHOW FILE TO LINK SOCKET.IO
 var io = require('socket.io').listen(server);
 
+var converted = 0;
+
 // When communication is established
 io.on('connection', function (socket) {
     socket.on('readSensor', function handleSensor() {
-		const child = execFile('./greenhouse', (error, stdout, stderr) => {
-		if(error) {
-			console.error('stderr', stderr);
-			throw error;
-		}
-		console.log(stdout);
-		// Cut out temperature and humidity values from sensor output
-		var posHumidity = stdout.indexOf("H: ") + 3;
-		var humi = stdout.slice(posHumidity, posHumidity+3);
-		var posTemp = stdout.indexOf("T: ") + 3;
-		var temp = stdout.slice(posTemp, posTemp+2);
-		// Create and send JSON object to browser
-		var data = {temperature: temp, humidity: humi};
-		var dataJSON = JSON.stringify(data);
-		io.emit('sensorData', dataJSON);
-		});
-	});
+        const child = execFile('./greenhouse', (error, stdout, stderr) => {
+            if (error) {
+                console.error('stderr', stderr);
+                throw error;
+            }
+            console.log(stdout);
+            // Cut out temperature and humidity values from sensor output
+            var posHumidity = stdout.indexOf("H: ") + 3;
+            var humi = stdout.slice(posHumidity, posHumidity + 3);
+            var posTemp = stdout.indexOf("T: ") + 3;
+            var temp = stdout.slice(posTemp, posTemp + 2);
+            // Create and send JSON object to browser
+            var data = { temperature: temp, humidity: humi };
+            var dataJSON = JSON.stringify(data);
+            io.emit('sensorData', dataJSON);
+        });
+    });
+    socket.on('readBrightness', function handleSensor() {
+        fs.readFile('/sys/bus/iio/devices/iio\:device0/in_voltage0_raw', 'utf8', function (err, contents) {
+            console.log(contents);
+            // /sys/bus/iio/devices/iio\:device0/in_voltage0_raw
+            // darkest 4080 110
+            // brightest 3970 0
+
+            converted = Math.ceil(Math.abs((contents - 3970) / 110 * 100 - 100));
+
+            var data = { brightness: converted };
+            var dataJSON = JSON.stringify(data);
+            io.emit('brightnessSensor', dataJSON);
+        });
+    });
+    socket.on('moveMotor', ({ value }) => {
+        moveMotor(value);
+    });
+    socket.on('adjustBrightness', ({ value }) => {
+        adjustBrightness(value);
+    });
+    socket.on('toggleHeating', ({ value }) => {
+        toggleHeating(value);
+    });
+    socket.on('autoBrightness', () => {
+        autoBrightness();
+    });
 });
+
+function moveMotor(value) {
+    const child = execFile('./motor', [value], (error, stdout, stderr) => {
+        if (error) {
+            console.error('stderr', stderr);
+            throw error;
+        }
+    });
+}
+
+function adjustBrightness(value) {
+    const child = execFile('./led', [value], (error, stdout, stderr) => {
+        if (error) {
+            console.error('stderr', stderr);
+            throw error;
+        }
+    });
+}
+
+function autoBrightness() {
+    const child = execFile('./led', [Math.abs(converted - 100)], (error, stdout, stderr) => {
+        if (error) {
+            console.error('stderr', stderr);
+            throw error;
+        }
+    });
+}
+
+function toggleHeating(value) {
+    let h = value ? 1 : 0;
+    const child = execFile('./led_gpio', [h], (error, stdout, stderr) => {
+        if (error) {
+            console.error('stderr', stderr);
+            throw error;
+        }
+    });
+}
 
 // Displaying a console message for user feedback
 server.listen(console.log("Server Running ..."));
